89282eb7c00e4797194471eb8e86b7bea8448506
Ryan Pinkstaff <rdpinkstaff@gmail.com>
2023-12-31 15:35:17 -0700

    grid with colors complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import TitleTile from './TitleTile'
 import ColorTiles from './ColorTiles'
 import './App.css'
 
 function App() {
+  const [tiles, setTiles] = useState<{}[]>([]);
+
+  useEffect(() => {
+    let arr = []
+    for(let i = 0; i < 28; i++) {
+      arr.push(<ColorTiles key={i} />)
+    }
+    setTiles(arr);
+  }, [])
+
 
   return (
     <>
-     <TitleTile />
-     <ColorTiles />
+      <div className='tile-container'>
+        <TitleTile />
+        {tiles}
+      </div>
+     
     </>
   )
 }
--- src/ColorTiles.tsx
+++ src/ColorTiles.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState, useRef } from "react";
 import "./colorTiles.css"
 
+const HEXVALUES = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+
 function ColorTiles() {
+    const [color, setColor] = useState<string>("");
+    const windowRef = useRef<number[]>([window.innerHeight, window.innerWidth]) 
+    
+    const style = { 
+        backgroundColor: color,
+    }
+
+    useEffect(() => {
+        createHex()
+    },[])
+
+    function createHex() {
+        let val = "#";
+        
+        while(val.length < 7) {
+          let index = Math.floor(Math.random() * HEXVALUES.length);
+          val += HEXVALUES[index];
+        }
+        setColor(val);
+      }
+
     return (
-        <div className="color-tile">
-            <p className="color-name">Some Color</p>
+        <div className="color-tile" style={style}>
+            <p className="color-name">{color}</p>
         </div>
     )
 }
